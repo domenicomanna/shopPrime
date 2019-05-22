3b09c54b75c8877b446c431a4b86d20b057d42f3
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-22 15:26:28 -0400

    Add disabled button style

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,9 +4,11 @@ import styles from './Button.module.css';
 const Button = (props) => {
     let buttonClasses = `${styles.button} ${styles[props.buttonType]}`;
 
-    return ( 
-        <button className = {buttonClasses}> {props.children} </button>
-     );
+    return (
+        <button
+            className={buttonClasses}> {props.children}
+        </button>
+    );
 }
- 
+
 export default Button;
