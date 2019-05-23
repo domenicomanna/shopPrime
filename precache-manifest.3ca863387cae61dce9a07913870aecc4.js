096ed5b3083d8c93b182abbc56d1bbc1216c3327
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-23 19:05:26 -0400

    Updates

---
+++ precache-manifest.3ca863387cae61dce9a07913870aecc4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c176537c6445618820e64f8701aa161b",
+    "url": "/shopPrime/index.html"
+  },
+  {
+    "revision": "bc04757fb4592370433c",
+    "url": "/shopPrime/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "fc5d31f4449c6856ffba",
+    "url": "/shopPrime/static/css/main.57c723e5.chunk.css"
+  },
+  {
+    "revision": "bc04757fb4592370433c",
+    "url": "/shopPrime/static/js/2.330d833e.chunk.js"
+  },
+  {
+    "revision": "fc5d31f4449c6856ffba",
+    "url": "/shopPrime/static/js/main.ce992cff.chunk.js"
+  },
+  {
+    "revision": "8c331af4b8499bcb4ceb",
+    "url": "/shopPrime/static/js/runtime~main.7e068ea3.js"
+  },
+  {
+    "revision": "b2d49644972ab68a89baea5ab936a085",
+    "url": "/shopPrime/static/media/blackSuit.b2d49644.jpg"
+  },
+  {
+    "revision": "b12287eca6235194901432937b6e7f1a",
+    "url": "/shopPrime/static/media/blackSuitJacket.b12287ec.jpg"
+  },
+  {
+    "revision": "a65b12c3b1a36e1d3462f03ec157d6e4",
+    "url": "/shopPrime/static/media/blackSuitWithBowtie.a65b12c3.jpg"
+  },
+  {
+    "revision": "5238d1463fd8c19911bf09c4a5369b9a",
+    "url": "/shopPrime/static/media/blueShirt.5238d146.jpg"
+  },
+  {
+    "revision": "a821a5b06b992fca13f1675bf785b7c6",
+    "url": "/shopPrime/static/media/blueSuit.a821a5b0.jpg"
+  },
+  {
+    "revision": "bdf2a36334e7e5e14de15abf06f6779b",
+    "url": "/shopPrime/static/media/crewWhite.bdf2a363.jpg"
+  },
+  {
+    "revision": "548d9cd9cb0221d56f16a7d5938cfe6b",
+    "url": "/shopPrime/static/media/greyCollarShirt.548d9cd9.jpg"
+  },
+  {
+    "revision": "737c7cd7554664c799cc5377574c9c19",
+    "url": "/shopPrime/static/media/greySuitJacket.737c7cd7.jpg"
+  },
+  {
+    "revision": "25d29dc9b60ec219140e55d3d36f8b97",
+    "url": "/shopPrime/static/media/leatherJacket.25d29dc9.jpg"
+  },
+  {
+    "revision": "2240a6e5aeb0e444014d8dfb19bf7afc",
+    "url": "/shopPrime/static/media/rainZipUp.2240a6e5.jpg"
+  },
+  {
+    "revision": "21618fcf69f8ca3b31021835c9613830",
+    "url": "/shopPrime/static/media/simpsonsShirt.21618fcf.jpg"
+  },
+  {
+    "revision": "32cfd6f5766079c13ba9c7a76e34b69d",
+    "url": "/shopPrime/static/media/vest.32cfd6f5.jpg"
+  }
+]);
