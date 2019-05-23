bb8b318fd59e869cf40046980a5bce3d85275376
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-23 11:48:35 -0400

    Add favicon

--- src/containers/ProductContainer/ProductContainer.js
+++ src/containers/ProductContainer/ProductContainer.js
@@ -34,7 +34,7 @@ class ProductContainer extends Component {
     displayToast() {
         toast('Item added to cart. Checkout at the bottom of the page', {
             position: "top-center",
-            autoClose: 5000,
+            autoClose: 3000,
             className: css({
                 color: '#000',
                 borderRadius: '5px'
